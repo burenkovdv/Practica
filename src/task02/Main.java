ea4f0e35ab026749c54c9fec91224415a8018c84
Dmitriy <burenkov@mail.ru>
2025-07-06 11:18:01 +0300

    Учебный комит

--- src/task02/Main.java
+++ src/task02/Main.java
@@ -1,7 +1,20 @@
 package task02;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> a = List.of("apple", "banana", "pear", "apple");
+        List<String> b = List.of("banana", "kiwi", "apple");
+        System.out.println(difference(a, b)); // ожидается [pear]
 
+    }
+
+    public static Set<String> difference(List<String> list1, List<String> list2) {
+        Set<String> res = new HashSet<>(list1);
+        res.removeAll(list2);
+        return res;
     }
 }
