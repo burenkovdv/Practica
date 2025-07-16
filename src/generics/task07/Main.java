15411c4d0709887444b5a03c5a84d43e174d72e6
Dmitriy <burenkov@mail.ru>
2025-07-16 23:31:23 +0300

    Класс переменного типа

--- src/generics/task07/Main.java
+++ src/generics/task07/Main.java
@@ -1,35 +1,73 @@
 package generics.task07;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] x = {1, 8, 4, 2, 7};
-        String[] str = {"red", "white", "green", "yellow"};
-        SimpleStack<String> p1 = new SimpleStack<>(new String[]{"red", "white", "green", "yellow"});
-        SimpleStack<String> p2 = new SimpleStack<>(str);
-        SimpleStack<String> p3 = new SimpleStack<>();
+
+        SimpleStack<String> p1 = new SimpleStack<>();
+        SimpleStack<Integer> q1 = new SimpleStack<>();
+
+        System.out.println(p1.isEmpty());
+        p1.push("Red");
+        p1.push("green");
+        q1.push(3);
+        q1.push(10);
+        q1.push(8);
+
+        System.out.println(q1.getItems());
+        System.out.println(q1.pop());
+        System.out.println(q1.getItems());
 
 
     }
 
-    class SimpleStack<T> {
-        T[] items;
+    static class SimpleStack<T> {
+        private T[] items;
+
+    public SimpleStack() {
+        this.items = (T[]) new Object[0];
+    }
 
 
-        public SimpleStack(T[] items) {
-            this.items = Arrays.copyOf(items, items.length);
-        }
-
-        SimpleStack(){};
-
-
-
+        //Добовляем элемент в массив
         void push(T item) {
             T[] result = Arrays.copyOf(items, items.length + 1);
+
             result[result.length - 1] = item;
             items = Arrays.copyOf(result, result.length);
 
+        }
+
+        public String getItems() {
+            return Arrays.toString(items);
+        }
+
+        //Удаляем последний элемент из массива и возвращаем его
+        T pop() {
+            if (items.length == 0) throw new NoSuchElementException("Массив пустой");
+            T res = items[items.length - 1];
+            T[] result = Arrays.copyOf(items, items.length - 1);
+            items = Arrays.copyOf(result, result.length);
+            return res;
+        }
+
+        //Возвращает последний элемент массива
+        T peek() {
+            if (items.length == 0) throw new NoSuchElementException("Массив пустой");
+            return items[items.length - 1];
+        }
+
+        //Проверяем есть ли в массиве элементы
+        boolean isEmpty() {
+            return items.length > 0 ? false : true;
+        }
+
+        //Возвращаем текущее числов элементов в стэке
+        int size() {
+            return items.length;
         }
 
     }
