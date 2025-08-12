94a22b0baaa5f650b3de3ebc25f81059003eea61
landsreyk <mr.andrey.kozyrev@gmail.com>
2025-08-12 20:23:46 +0300

    -- правки по фильтру-трансформатору

--- src/main/java/lambda_streamapi/task04/Main.java
+++ src/main/java/lambda_streamapi/task04/Main.java
@@ -2,7 +2,6 @@ package lambda_streamapi.task04;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +9,9 @@ public class Main {
         List<Integer> list = filterAndTransform(words, x -> x.length() <= 3, String::length);
         list.forEach(System.out::println);
 
-        List<String> list1 = filterAndTransform(words, x -> !x.substring(0, 1).contains("b") && !x.substring(0, 1).contains("f"),
+        List<String> list1 = filterAndTransform(words, x -> x.charAt(0) == 'b' || x.charAt(0) == 'f',
                 String::toUpperCase);
         list1.forEach(System.out::println);
-
-
     }
 
     public static <T, R> List<R> filterAndTransform(
@@ -22,15 +19,14 @@ public class Main {
             java.util.function.Predicate<T> filter,
             java.util.function.Function<T, R> transformer
     ) {
-        List<T> result = new ArrayList<>(list);
-        result.removeIf(filter);
-        Function<T, R> func = transformer;
-        List<R> result1 = new ArrayList<>();
+        List<R> result = new ArrayList<>();
 
-        for (T element : result) {
-            result1.add(func.apply(element));
+        for (T t : list) {
+            if (filter.test(t)) {
+                result.add(transformer.apply(t));
+            }
         }
-        return result1;
+        return result;
     }
 
 }
