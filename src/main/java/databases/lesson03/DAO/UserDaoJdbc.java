b62a697b51e2419bb37b567afcafbe8b30c92283
Dmitriy <burenkov@mail.ru>
2025-09-20 21:25:41 +0300

    Покупки 1.1

--- src/main/java/databases/lesson03/DAO/UserDaoJdbc.java
+++ src/main/java/databases/lesson03/DAO/UserDaoJdbc.java
@@ -8,7 +8,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoJdbc implements UserDao {
 
@@ -30,6 +32,7 @@ public class UserDaoJdbc implements UserDao {
                                FROM users u
                                LEFT JOIN orders o ON u.id = o.user_id
                                WHERE u.id = ?
+                               ORDER BY o.id
                 """;
 
         PreparedStatement preparedStatement = connection.prepareStatement(query);
@@ -64,42 +67,45 @@ public class UserDaoJdbc implements UserDao {
 
     @Override
     public List<User> findAllWithOrders() throws SQLException {
-        List<Order> list = new ArrayList<>();
-        List<User> rezult = new ArrayList<>();
 
         String query = """
                 SELECT u.id AS user_id, u.name, o.id AS order_id, o.product as product, o.amount as amount
                 FROM users u
                 LEFT JOIN orders o ON u.id = o.user_id
+                ORDER BY u.id, o.id
                 """;
 
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         ResultSet resultSet = preparedStatement.executeQuery();
 
-        Integer idUser = 0;
-        String nameUser = "";
+        Map<Integer, User> userMap = new LinkedHashMap<>();
+
         while (resultSet.next()) {
+            int userId = resultSet.getInt("user_id");
+            User user = userMap.computeIfAbsent(userId, id -> {
+                User u = new User();
+                u.setId(userId);
+                try {
+                    u.setName(resultSet.getString("name"));
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                u.setOrders(new ArrayList<>());
+                return u;
+            });
 
-            Order temp = new Order(
-                    resultSet.getInt("order_id"),
-                    resultSet.getInt("user_id"),
-                    resultSet.getString("product"),
-                    resultSet.getInt("amount")
-            );
-            if (idUser != temp.getUserId() && idUser != 0) {
-                User user = new User(idUser, nameUser, new ArrayList<>(list));
-                rezult.add(user);
-                list.clear();
-            }
-            list.add(temp);
-            idUser = temp.getUserId();
-            nameUser = resultSet.getString("name");
-
-            if (resultSet.isLast()) {
-                User user = new User(idUser, nameUser, new ArrayList<>(list));
-                rezult.add(user);
+            int orderId = resultSet.getInt("order_id");
+            if (!resultSet.wasNull()) {
+                Order order = new Order(
+                        orderId,
+                        userId,
+                        resultSet.getString("product"),
+                        resultSet.getInt("amount")
+                );
+                user.getOrders().add(order);
             }
         }
-        return rezult;
+
+        return new ArrayList<>(userMap.values());
     }
 }
